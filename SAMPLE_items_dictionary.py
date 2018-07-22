17449be57fc04c7f08e96ce35034735b08f3de03
Eric <eric.kumlin@gmail.com>
2018-07-22 13:09:09 -0700

    Add sample items list

---
+++ SAMPLE_items_dictionary.py
@@ -0,0 +1,32 @@
+items_list = {
+	"category1": [
+		[ "Do thing A" ],
+		[ "Do thing B" ],
+		[ "Do thing C" ],
+		[ "Do thing D" ],
+		[ "Do thing E" ],
+		[ "Do thing F" ],
+		[ "Do thing G" ],
+		[ "Do thing H" ],
+		[ "Do thing I" ],
+		[ "Do thing J" ],
+		[ "Do thing I" ],
+		[ "Do thing K" ],
+		[ "Do thing L" ],
+		[ "Do thing M" ],
+	],
+	"category2": [
+		[ "Do thing N" ],
+		[ "Do thing O" ],
+		[ "Do thing P" ],
+		[ "Do thing Q" ],
+	],
+	"category3": [
+		[ "Do thing R" ],
+		[ "Do thing S" ],
+		[ "Do thing T" ],
+		[ "Do thing U" ],
+		[ "Do thing V" ],
+		[ "Do thing W" ],
+	],
+}
